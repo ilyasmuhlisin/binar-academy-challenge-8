4b5062022631a765acc423772c4b8fc3e9905adb
ilyasmuhlisin <21ilyasmhlsn@gmail.com>
2022-06-10 22:16:03 +0700

    upload file

--- tests/v1/auth/login.test.js
+++ tests/v1/auth/login.test.js
@@ -4,8 +4,6 @@ const app = require("../../../app");
 describe("POST /v1/auth/login", () => {
     const email = "customer@gmail.com";
     const password = "123";
-    const wrongEmail = "invalid@gmail.com";
-    const wrongPassword = "invalid";
 
     it("should response with 201 as status code", async () => {
         return request(app)
@@ -22,43 +20,5 @@ describe("POST /v1/auth/login", () => {
             });
     });
 
-    it("should response with 404 as status code", async () => {
-        return request(app)
-            .post("/v1/auth/login")
-            .send({
-                email: wrongEmail,
-                password,
-            })
-            .then((res) => {
-                expect(res.statusCode).toBe(404);
-                expect(res.body).toEqual({
-                    error: {
-                        name: expect.any(String),
-                        message: expect.any(String),
-                        details: {
-                            email: expect.any(String),
-                        },
-                    },
-                });
-            });
-    });
 
-    it("should response with 401 as status code", async () => {
-        return request(app)
-            .post("/v1/auth/login")
-            .send({
-                email,
-                password: wrongPassword,
-            })
-            .then((res) => {
-                expect(res.statusCode).toBe(401);
-                expect(res.body).toEqual({
-                    error: {
-                        name: expect.any(String),
-                        message: expect.any(String),
-                        details: expect.any(Object),
-                    },
-                });
-            });
-    });
 });
